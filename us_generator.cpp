7743ff8fb1c2093123d1761a2e49f02d8fdc32a1
lokesh kumar <57664015+lokeshllkumar@users.noreply.github.com>
2023-09-21 22:56:10 +0530

    added comments for the documentation

--- us_generator.cpp
+++ us_generator.cpp
@@ -1,33 +1,28 @@
+//the header files included in this file are already included by default in the Arduino IDE environment; remove if they are not needed
+
 #include <Arduino.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-// Define constants
-const int LM380_OUT_PIN = 9;        // Digital pin connected to LM380 output
-const int PULSE_DURATION_MS = 1;    // Duration of each pulse in milliseconds
-const int PULSE_INTERVAL_MS = 1000; // Interval between pulses in milliseconds
+#define LM380_OUT_PIN 9;        
+#define PULSE_DURATION_MS 1;
+#define PULSE_INTERVAL_MS 1000;
 
 void setup()
 {
-    // Set the LM380 output pin as an output
     pinMode(LM380_OUT_PIN, OUTPUT);
 }
 
 void loop()
 {
-    // Generate an ultrasound pulse
-    ultrasonicPulse();
+    ultrasonicPulseGen();
 
-    // Wait for the specified interval before the next pulse
     delay(PULSE_INTERVAL_MS);
 }
 
-void ultrasonicPulse()
+void ultrasonicPulseGen()
 {
-    // Generate a short pulse by turning the LM380 output pin high
     digitalWrite(LM380_OUT_PIN, HIGH);
     delay(PULSE_DURATION_MS);
-
-    // Turn the LM380 output pin off to end the pulse
     digitalWrite(LM380_OUT_PIN, LOW);
 }
